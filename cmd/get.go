a8105721e46b8dc0bafca5b7d404301a7290a8a7
Zhong Wei <zhongwei99@163.com>
2017-01-11 11:21:57 +0800

    support input multiple url one time

--- cmd/get.go
+++ cmd/get.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
+	"reflect"
 
 	"github.com/spf13/cobra"
 	"github.com/zhongwei/goreader/netdata"
@@ -9,23 +10,27 @@ import (
 )
 
 var (
-    url string
+	urls []string
 )
 
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "A brief description of your command",
-	Long: ` and usage of using your command. For example: Cobra is a CLI library for Go that empockly create a Cobra application.`,
+	Long:  ` and usage of using your command. For example: Cobra is a CLI library for Go that empockly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		    log.Println("get content from : " + url)
-        siteContent := netdata.Get(url)
-        store.Save(url, siteContent)
+		for no, url := range urls {
+			fmt.Printf("get content from index : %d\n", no)
+			fmt.Println("type:", reflect.TypeOf(no))
+			fmt.Println("get content from : " + url)
+			siteContent := netdata.Get(url)
+			store.Save(url, siteContent)
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(getCmd)
-  getCmd.PersistentFlags().String("foo", "", "A help for foo")
-  getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-  getCmd.Flags().StringVarP(&url, "url", "u", "", "Get site url address.")
+	getCmd.PersistentFlags().String("foo", "", "A help for foo")
+	getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	getCmd.Flags().StringSliceVarP(&urls, "urls", "u", []string{}, "Get site url address.")
 }
